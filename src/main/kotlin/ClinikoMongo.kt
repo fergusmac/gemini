baf1110b4904f5d6911a8bc220f149a8d30c0cda
Fergus <fdmacpherson@gmail.com>
2023-11-17 15:27:53 +1100

    Got a nice, reasonably clean insert / update function for patients into mongo

--- src/main/kotlin/ClinikoMongo.kt
+++ src/main/kotlin/ClinikoMongo.kt
@@ -5,6 +5,7 @@ import com.mongodb.MongoException
 import com.mongodb.client.model.Filters
 import com.mongodb.client.model.Filters.eq
 import com.mongodb.client.model.Filters.`in`
+import com.mongodb.client.model.ReplaceOptions
 import com.mongodb.client.model.UpdateOptions
 import com.mongodb.client.model.Updates
 import com.mongodb.kotlin.client.coroutine.MongoClient
@@ -13,6 +14,7 @@ import io.github.oshai.kotlinlogging.KotlinLogging
 import kotlinx.coroutines.flow.firstOrNull
 import kotlinx.coroutines.flow.toList
 import org.bson.codecs.configuration.CodecRegistries
+import kotlin.reflect.KCallable
 
 private val logger = KotlinLogging.logger {}
 class ClinikoMongo (connectionString: ConnectionString, val databaseName : String) {
@@ -32,19 +34,25 @@ class ClinikoMongo (connectionString: ConnectionString, val databaseName : Strin
 
     suspend fun addOrUpdatePatient(clinikoPatient : ClinikoPatient) {
 
-        val query = Filters.eq(Patient::cliniko.name + "." + ClinikoObject::id.name, clinikoPatient.id)
-        val updates = Updates.combine(
-            Updates.set(Patient::marketingSource.name, clinikoPatient.referralSource),
-            //TODO rest of the fields
-            /*Updates.addToSet(Movie::genres.name, "Sports"),
-            Updates.currentDate(Movie::lastUpdated.name)*/
-        )
-        val options = UpdateOptions().upsert(true)
+        //TODO some sort of transaction lock between getting and setting?
+        val existing = getPatient(clinikoPatient.id)
+
+        //copy any non-cliniko fields from the existing document (if any)
+        val updated = Patient.fromCliniko(clinikoPatient, existing=existing)
+
+        if (updated == existing) {
+            return
+        }
 
         try {
-            patients.updateOne(query, updates, options)
-        } catch (e: MongoException) {
-            logger.error { "Unable to update cliniko patient ${clinikoPatient.id} due to an error: $e" }
+            patients.replaceOne(
+                filter = eq(Patient::id.name, updated.id),
+                replacement = updated,
+                options = ReplaceOptions().upsert(true)
+            )
+        }
+        catch (e: MongoException) {
+            logger.error { "Unable to insert/update cliniko patient ${clinikoPatient.id} due to an error: $e" }
         }
     }
 
@@ -57,3 +65,7 @@ class ClinikoMongo (connectionString: ConnectionString, val databaseName : Strin
     }
 }
 
+
+infix fun String.dot(other: String) : String {
+    return "$this.$other"
+}
