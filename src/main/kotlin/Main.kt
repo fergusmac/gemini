af6ea10b77670cfbba852012caca452caeed33f4
Fergus <fdmacpherson@gmail.com>
2023-12-23 06:36:58 +1100

    Run cliniko fetch functions in parallel

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,9 @@
 import cliniko.ClinikoClient
 import com.mongodb.ConnectionString
 import io.github.oshai.kotlinlogging.KotlinLogging
+import kotlinx.coroutines.async
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.runBlocking
 import mongo.ClinikoMongoAdapter
 import mongo.MongoWrapper
 import mongo.SqlMigrator
@@ -8,7 +11,7 @@ import java.io.File
 import java.util.*
 
 private val logger = KotlinLogging.logger {}
-suspend fun main(args: Array<String>) {
+fun main(args: Array<String>) {
 
 
     val mongo = MongoWrapper(ConnectionString("mongodb://localhost:27017"), databaseName = "constellation")
@@ -21,30 +24,42 @@ suspend fun main(args: Array<String>) {
     val clinikoApiKey : String by localProperties
     val cliniko = ClinikoClient("api.au3.cliniko.com", apiKey = clinikoApiKey)
 
-    val patients = cliniko.getPatients()//params = parametersOf("q[]", "id:=826332652180085831"))
-    val cases = cliniko.getCases()
-    //cliniko.getContacts()
-    val appts = cliniko.getAppointments()
-    //cliniko.getGroupAppts()
+    runBlocking {
+        coroutineScope {
 
-    //cliniko.getUnavailabilities()
-    val attendees = cliniko.getAttendees()
-    /*cliniko.getApptTypes()
-    cliniko.getAvailabilities()
-    cliniko.getGroupAppts()*/
-    val practs = cliniko.getPractitioners()
-    val users = cliniko.getUsers()
-    val prns = cliniko.getPractNumbers()
-    //cliniko.getBusinesses()
+            //download all the rows from cliniko, asynchronously
+            val getPatients = async { cliniko.getPatients() }
+            val getCases = async { cliniko.getCases() }
+            val getContacts = async { cliniko.getContacts() }
+            val getAppts = async { cliniko.getAppointments() }
+            val getGroupAppts = async { cliniko.getGroupAppts() }
+            val getAttendees = async { cliniko.getAttendees() }
+            val getApptTypes = async { cliniko.getApptTypes() }
+            val getAvailabilities = async { cliniko.getAvailabilities() }
+            val getPractitioners = async { cliniko.getPractitioners() }
+            val getUsers = async { cliniko.getUsers() }
+            val getPractNumbers = async { cliniko.getPractNumbers() }
+            val getBusinesses = async { cliniko.getBusinesses() }
+            val getUnavailabilities = async { cliniko.getUnavailabilities() }
 
+            //getBusinesses.await().values.forEach { }
+            getApptTypes.await().values.forEach { adapter.addOrUpdateApptType(it) }
 
-    patients.values.forEach { adapter.addOrUpdatePatient(it) }
-    cases.values.forEach { adapter.updatePatientWithCase(it) }
-    appts.values.forEach { adapter.updatePatientWithAppt(it) }
-    attendees.values.forEach { adapter.updatePatientWithAttendee(it) }
-    practs.values.forEach { adapter.addOrUpdatePract(it) }
-    users.values.forEach { adapter.updatePractWithUser(it) }
-    prns.values.forEach { adapter.updatePractWithNumber(it) }
+            getPatients.await().values.forEach { adapter.addOrUpdatePatient(it) }
+            getCases.await().values.forEach { adapter.updatePatientWithCase(it) }
+            //getContacts.await().values.forEach { adapter. }
+            getAppts.await().values.forEach { adapter.updatePatientWithAppt(it) }
+            getAttendees.await().values.forEach { adapter.updatePatientWithAttendee(it) }
+            //getGroupAppts.await().values.forEach { }
+
+            getPractitioners.await().values.forEach { adapter.addOrUpdatePract(it) }
+            getUsers.await().values.forEach { adapter.updatePractWithUser(it) }
+            getPractNumbers.await().values.forEach { adapter.updatePractWithNumber(it) }
+
+            //getAvailabilities.await().values.forEach { }
+            //getUnavailabilities.await().values.forEach { }
+        }
+    }
 
     val sqlConnectionString : String  by localProperties
     val sqlMigrator = SqlMigrator(sqlConnectionString = sqlConnectionString, clinikoAdapter = adapter)
